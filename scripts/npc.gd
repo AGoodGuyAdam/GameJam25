fee68126877e12c0120a401969b7c6051ff0f538
Jake <dominik8655@gmail.com>
2025-02-06 20:03:09 +0100

    feat: make npcs talk

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -7,7 +7,9 @@ var dialogue_idx := 0
 
 
 func _on_texture_button_pressed() -> void:
-	print("hello")
-	print(dialogues)
+	if dialogue_idx >= dialogues.size():
+		dialogue_idx = 0
+		dialogue_label.text = ""
+		return
 	dialogue_label.text = dialogues[dialogue_idx]
 	dialogue_idx += 1
