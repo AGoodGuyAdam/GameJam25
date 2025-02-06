1786d4147d84adad17ae1c99056267f9b29ac54b
Jake <dominik8655@gmail.com>
2025-02-06 20:27:03 +0100

    feat(npc): proximity detection for clicks

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
 @onready var dialogue_label := $Text
+@onready var player := %Player
+
 @export var dialogues: Array[String] = []
 
 var dialogue_idx := 0
 
 
 func _on_texture_button_pressed() -> void:
+	print(player.position)
+	print(position.distance_to(player.position))
+
+	if position.distance_to(player.position) >= 50:
+		return
+
 	if dialogue_idx >= dialogues.size():
 		dialogue_idx = 0
 		dialogue_label.text = ""
