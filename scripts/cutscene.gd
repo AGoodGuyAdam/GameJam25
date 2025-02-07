77446654808bad88fe3a11b8878150f5b1ecd61c
Jake <dominik8655@gmail.com>
2025-02-07 11:55:42 +0100

    feat: end cutscene sound

--- scripts/cutscene.gd
+++ scripts/cutscene.gd
@@ -1,6 +1,9 @@
 extends AudioStreamPlayer2D
 
+@onready var end := $"/root/Game/Player/Camera2D/end"
 
-func _ready() -> void:
-	await get_tree().create_timer(3.5).timeout
-	play()
+
+func _process(_delta: float) -> void:
+	if end.visible:
+		await get_tree().create_timer(3.5).timeout
+		play()
