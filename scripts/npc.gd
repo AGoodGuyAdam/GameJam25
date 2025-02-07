d5c106cfd4eeba2aee404fe8df4a4e72db327e09
Jake <dominik8655@gmail.com>
2025-02-07 10:08:14 +0100

    feat: npc sounds

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -3,6 +3,7 @@ extends Node2D
 var MAGIC_DISTANCE := 954
 @export var magic_sprite_number := 0
 @export var dialogues: Array[String] = []
+@export var dialogue_stream: AudioStream
 
 var dialogue_idx := 0
 var talking := false
@@ -12,9 +13,12 @@ var already_talked := false
 @onready var texture_button: TextureButton = $TextureButton
 @onready var game := $"/root/Game"
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var dialogue_stream_player = $AudioStreamPlayer2D
 
 
 func _ready() -> void:
+	dialogue_stream_player.stream = dialogue_stream
+
 	if magic_sprite_number == 0:
 		animated_sprite.play("1")
 	elif magic_sprite_number == 1:
@@ -38,10 +42,12 @@ func _on_texture_button_pressed() -> void:
 
 	dialogue_label.text = ""
 	talking = true
+	dialogue_stream_player.play()
 	for ch in dialogues[dialogue_idx]:
 		dialogue_label.text += ch
 		await get_tree().create_timer(.1).timeout
 
+	dialogue_stream_player.stop()
 	talking = false
 
 	dialogue_idx += 1
