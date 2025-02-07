8ca36c70f50b100495c156c5950bba53acf2adb9
Jake <dominik8655@gmail.com>
2025-02-07 04:01:29 +0100

    feat: lever

--- scripts/lever.gd
+++ scripts/lever.gd
@@ -1,9 +1,8 @@
 extends StaticBody2D
 
-var MAGIC_DISTANCE := 730
-@export var to_position := Vector2.ZERO
+var MAGIC_DISTANCE := 935
 @export var required_stage := 0
-@onready var sprite = $Sprite2D
+@onready var animated_sprite = $AnimatedSprite2D
 @onready var player := %Player
 @onready var game := $"/root/Game"
 
@@ -12,7 +11,9 @@ func _on_texture_button_pressed() -> void:
 	if game.get_stage() < required_stage:
 		return
 
+	print(position.distance_to(player.position))
 	if position.distance_to(player.position) >= MAGIC_DISTANCE + 50:
 		return
 
-	player.position = to_position
+	animated_sprite.play("on")
+	game.increase_stage()
