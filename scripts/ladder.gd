1057be8c83be97ec965ec22f29737ba3394f8fdd
Jake <dominik8655@gmail.com>
2025-02-07 03:37:30 +0100

    feat: basic ladder

--- scripts/ladder.gd
+++ scripts/ladder.gd
@@ -1,8 +1,7 @@
 extends StaticBody2D
 
-var MAGIC_DISTANCE := 540
+@export var to_position := Vector2.ZERO
 @export var required_stage := 0
-@onready var collision_shape = $CollisionShape2D
 @onready var sprite = $Sprite2D
 @onready var player := %Player
 @onready var game := $"/root/Game"
@@ -12,9 +11,7 @@ func _on_texture_button_pressed() -> void:
 	if game.get_stage() <= required_stage:
 		return
 
-	if position.distance_to(player.position) >= MAGIC_DISTANCE + 50:
+	if position.distance_to(player.position) >= 50:
 		return
 
-	collision_shape.disabled = true
-	sprite.texture = preload("res://assets/door_open.png")
-	game.increase_stage()
+	player.position = to_position
