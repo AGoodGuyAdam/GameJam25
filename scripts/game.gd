a3705249367800e11c620716c5fc9d95fa302004
Jake <dominik8655@gmail.com>
2025-02-07 04:34:06 +0100

    feat: mob

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,8 +4,18 @@ var stage := 0
 @onready var alarm := $Alarm
 
 
+func spawn_mob(magic_positioooooono: Vector2):
+	var mob := preload("res://scenes/mob.tscn").instantiate()
+	mob.global_position = magic_positioooooono
+	add_child(mob)
+
+
 func increase_stage():
 	stage += 1
+
+	if stage == 4:
+		spawn_mob(Vector2(129, 945))
+
 	if stage == 1:
 		alarm.play()
 	else:
--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
-@onready var player: CharacterBody2D = %Player
+@onready var player: CharacterBody2D = $"/root/Game/Player"
 
-func _physics_process(delta: float) -> void:
+
+func _physics_process(_delta: float) -> void:
 	var dir = global_position.direction_to(player.global_position)
 	velocity = dir * 15
 	move_and_slide()
-	
