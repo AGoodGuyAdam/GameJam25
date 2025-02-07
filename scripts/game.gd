33baa39ad86c2b6933f0459a682bf2956cafe359
Jake <dominik8655@gmail.com>
2025-02-07 01:36:35 +0100

    feat: alarm sound

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var stage := 0
+@onready var alarm := $Alarm
 
 
 func _process(_delta: float) -> void:
@@ -9,7 +10,16 @@ func _process(_delta: float) -> void:
 
 func increase_stage():
 	stage += 1
+	if stage == 1:
+		alarm.play()
+	else:
+		alarm.stop()
 
 
 func get_stage():
 	return stage
+
+
+func _on_alarm_finished() -> void:
+	if stage == 1:
+		alarm.play()
